a9bcd41931c19115f48bcd5808c9882e1e6a2a81
xoumi <xoumi712@gmail.com>
2021-04-20 22:54:19 +0530

    add flyingfox, fix zsh startup

--- config/zsh/exports.zsh
+++ config/zsh/exports.zsh
@@ -14,11 +14,11 @@ export COMPLETION_WAITING_DOTS="true"
 
 export ZDOTDIR=$HOME/.cache/zsh/
 export HIST_STAMPS="dd.mm.yyyy"
-export HISTFILE=~/.cache/zsh/history
+export HISTFILE=$HOME/.cache/zsh/history
 export HISTSIZE=1000
 export SAVEHIST=1000
 
-export FIREFOX_DIR="/home/soma/.mozilla/firefox/asalsky0.default-release"
+export FIREFOX_DIR="/home/soma/.mozilla/firefox/v1n033gs.default-release-1612686329958"
 
 export KEYTIMEOUT=1
 export QT_AUTO_SCREEN_SCALE_FACTOR=0 zeal
