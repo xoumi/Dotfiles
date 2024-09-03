3ffdf3e3c387513f6fcee8eee131319ad8cb7ad5
xoumi <xoumi712@gmail.com>
2024-09-03 16:12:44 +0530

    add wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.font = wezterm.font 'JetBrainsMono NerdFont'
+config.font_size = 10
+config.line_height = 1.36
+config.freetype_load_target = "Light"
+config.freetype_render_target = "HorizontalLcd"
+config.enable_tab_bar = false
+config.window_padding = {
+  left = 20,
+  top = 20,
+  right = 20,
+  bottom = 20
+};
+
+config.color_scheme_dirs = {'/home/soma/.config/wezterm/colors'}
+-- config.color_scheme = 'pywal'
+
+config.window_background_opacity = 0.7;
+return config
